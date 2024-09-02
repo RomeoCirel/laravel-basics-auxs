fe4e05a2edfc1a65fdeab0261197ae0e34e0394f
Cirel H. Romeo <cirelhromeo@gmail.com>
2024-09-02 17:38:29 -0300

    Fix: copy files

--- src/LaravelBasicsAuxsServiceProvider.php
+++ src/LaravelBasicsAuxsServiceProvider.php
@@ -34,7 +34,22 @@ class LaravelBasicsAuxsServiceProvider extends ServiceProvider
             __DIR__ . '/Http/Requests/' => $paths['Requests'],
         ], 'laravel-basics-auxs');
 
+        $this->copyDirectoryContents($filesystem, __DIR__ . '/Auxs', $paths['Auxs']);
+        $this->copyDirectoryContents($filesystem, __DIR__ . '/Traits', $paths['Traits']);
+        $this->copyDirectoryContents($filesystem, __DIR__ . '/Handlers', $paths['Handlers']);
+        $this->copyDirectoryContents($filesystem, __DIR__ . '/Http/Requests', $paths['Requests']);
+
+
 
         $filesystem->copy(__DIR__ . '/Handlers/InvalidJsonResponseHandler.php', $paths['Handlers'] . '/InvalidJsonResponseHandler.php');
+    }
+
+    protected function copyDirectoryContents(Filesystem $filesystem, $sourceDir, $destinationDir)
+    {
+        $files = $filesystem->allFiles($sourceDir);
+
+        foreach ($files as $file) {
+            $filesystem->copy($file->getRealPath(), $destinationDir . '/' . $file->getFilename());
+        }
     }
 }
